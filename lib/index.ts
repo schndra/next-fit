a291ea8cd1320fb32c3d7c55f1ab3f58009e1e17
schndra <ardnihc@gmail.com>
2025-08-09 08:40:48 +0530

    add layout and page components with metadata configuration

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Next Fit";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A modern commerce store build with nextjs";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
